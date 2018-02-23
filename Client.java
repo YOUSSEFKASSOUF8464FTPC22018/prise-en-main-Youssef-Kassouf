98e60811806bc906b875b258ee1b756a2a862c8f
Youssef Kassouf <youssef.kassouf@isae.edu.lb>
2018-02-23 18:45:11 +0200

    Class Client avec Builder

--- Client.java
+++ Client.java
@@ -1,56 +1,136 @@
 public class Client{
-    private string _id;
-    private string _prenom;
-    private string _nom;
-    private string _telephone;
-    private string _rue;
-    private string _ville;
-    private string _etat;
-    private string _code;
-    private string _pays;
-    private string _mail;
+    private String _id;
+    private String _prenom;
+    private String _nom;
+    private String _telephone;
+    private String _rue;
+    private String _ville;
+    private String _etat;
+    private String _code;
+    private String _pays;
+    private String _mail;
 
-    public Client(DataBuilder db){
-        _id = db._id;
-        _prenom = db._prenom;
-        _nom = db._nom;
-        _telephone = db._telephone;
-        _rue = db._rue;
-        _ville = db._ville;
-        _etat = db._etat;
-        _code = db._code;
-        _pays = db._pays;
-        _mail = db._mail;
+    public Client(ClientBuilder cb){
+        _id = cb._id;
+        _prenom = cb._prenom;
+        _nom = cb._nom;
+        _telephone = cb._telephone;
+        _rue = cb._rue;
+        _ville = cb._ville;
+        _etat = cb._etat;
+        _code = cb._code;
+        _pays = cb._pays;
+        _mail = cb._mail;
     }
 
-    public string getId(){
+    public String getId(){
         return _id;
     }
-    public string getPrenom(){
+    public String getPrenom(){
         return _prenom;
     }
-    public string getNom(){
+    public String getNom(){
         return _nom;
     }
-    public string getTelephone(){
+    public String getTelephone(){
         return _telephone;
     }
-    public string getRue(){
+    public String getRue(){
         return _rue;
     }
-    public string getVille(){
+    public String getVille(){
         return _ville;
     }
-    public string getEtat(){
+    public String getEtat(){
         return _etat;
     }
-    public string getCode(){
+    public String getCode(){
         return _code;
     }
-    public string getPays(){
+    public String getPays(){
         return _pays;
     }
-    public string getMail(){
+    public String getMail(){
         return _mail;
+    }
+
+    public void setPrenom(String prenom){
+        _prenom = prenom;
+    }
+    public void setNom(String nom){
+        _nom = nom;
+    }
+    public void setTelephone(String telephone){
+        _telephone = telephone;
+    }
+    public void setRue(String rue){
+        _rue = rue;
+    }
+    public void setVille(String ville){
+        _ville = ville;
+    }
+    public void setEtat(String etat){
+        _etat = etat;
+    }
+    public void setCode(String code){
+        _code = code;
+    }
+    public void setPays(String pays){
+        _pays = pays;
+    }
+    public void setMail(String mail){
+        _mail = mail;
+    }
+
+    public static class ClientBuilder {
+        String _id;
+        String _prenom;
+        String _nom;
+        String _telephone;
+        String _rue;
+        String _ville;
+        String _etat;
+        String _code;
+        String _pays;
+        String _mail;
+
+        public ClientBuilder(String id, String prenom, String nom){
+            _id = id;
+            _prenom = prenom;
+            _nom = nom;
+        }
+
+        public ClientBuilder telephone (String telephone){
+            _telephone = telephone;
+            return this;
+        }
+        public ClientBuilder rue (String rue){
+            _rue = rue;
+            return this;
+        }
+        public ClientBuilder ville (String ville){
+            _ville = ville;
+            return this;
+        }
+        public ClientBuilder etat (String etat){
+            _etat = etat;
+            return this;
+        }
+        public ClientBuilder code (String code){
+            _code = code;
+            return this;
+        }
+        public ClientBuilder pays (String pays){
+            _pays = pays;
+            return this;
+        }
+        public ClientBuilder mail (String mail){
+            _mail = mail;
+            return this;
+        }
+
+        public Client build(){
+            return new Client(this);
+        }
     }
 }
